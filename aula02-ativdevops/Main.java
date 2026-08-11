f8939562469332686fbe86f0e945229820013993
danielvieira95 <engdanielvieira95@gmail.com>
2026-08-11 15:56:27 -0300

    pratica 01 Devops

---
+++ aula02-ativdevops/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, DevOps!");
+        System.out.println("Pipeline executada com sucesso!");
+    }
+}
